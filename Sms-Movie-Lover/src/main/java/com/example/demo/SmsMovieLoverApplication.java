1812f04d61de9ffc9757ef1a7f2e726f50d8a0d5
lanzoking <lanzoking@gmail.com>
2019-04-10 11:10:05 -0500

    Added cars to the database

--- Sms-Movie-Lover/src/main/java/com/example/demo/SmsMovieLoverApplication.java
+++ Sms-Movie-Lover/src/main/java/com/example/demo/SmsMovieLoverApplication.java
@@ -3,7 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//import com.movie.comtroller.MovieController;
+ import com.movie.comtroller.MovieController;
 
 
 @SpringBootApplication
@@ -11,14 +11,15 @@ public class SmsMovieLoverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmsMovieLoverApplication.class, args);
-//		MovieController controller = new MovieController();
-//		String movieName = "Focus";
-//		String movieYear = "2019";
-//		double raterRating = 7.3;
-//		controller.addMovie(movieName, movieYear);
-//		controller.addGenre(movieName, movieYear, "Comedy");
-//		controller.addCast(movieName, movieYear, "Bruce", "Willis", "John");
-//		controller.addPublisher(movieName, movieYear, "Warner Bros");
-//		controller.addRater(movieName, movieYear, "Rotten Tomatoes", raterRating);
+		MovieController controller = new MovieController();
+		String movieName = "Cars";
+		String movieYear = "2017";
+		double raterRating = 7.5;
+		controller.addMovie(movieName, movieYear);
+		controller.addGenre(movieName, movieYear, "Comedy");
+		controller.addGenre(movieName, movieYear, "Action");
+		controller.addCast(movieName, movieYear, "Tom", "Cruise", "Car");
+		controller.addPublisher(movieName, movieYear, "Disney");
+		controller.addRater(movieName, movieYear, "IMDB", raterRating);
 	}
 }//end class
