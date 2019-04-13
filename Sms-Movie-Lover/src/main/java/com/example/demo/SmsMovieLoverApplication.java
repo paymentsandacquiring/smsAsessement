db16902d092ebb7a519a07ea4675a22add96026f
lanzo King <lanzoking@gmail.com>
2019-04-13 00:43:01 -0500

    editted for a reason

--- Sms-Movie-Lover/src/main/java/com/example/demo/SmsMovieLoverApplication.java
+++ Sms-Movie-Lover/src/main/java/com/example/demo/SmsMovieLoverApplication.java
@@ -13,15 +13,15 @@ public class SmsMovieLoverApplication {
 		SpringApplication.run(SmsMovieLoverApplication.class, args);
 		MovieController controller = new MovieController();
 //		controller.getAllMovies();
-//		String movieName = "Fairy Tale";
-//		String movieYear = "1999";
-//		double raterRating = 9.0;
-//		controller.addMovie(movieName, movieYear);
-//		controller.addGenre(movieName, movieYear, "Comedy");
-//		controller.addGenre(movieName, movieYear, "Action");
-//		controller.addGenre(movieName, movieYear, "Fantasy");
-//		controller.addCast(movieName, movieYear, "Arya", "Stark", "Maisie");
-//		controller.addPublisher(movieName, movieYear, "HBO");
-//		controller.addRater(movieName, movieYear, "IMDB", raterRating);
+		String movieName = "Fairy Tale";
+		String movieYear = "1999";
+		double raterRating = 9.0;
+		controller.addMovie(movieName, movieYear);
+		controller.addGenre(movieName, movieYear, "Comedy");
+		controller.addGenre(movieName, movieYear, "Action");
+		controller.addGenre(movieName, movieYear, "Fantasy");
+		controller.addCast(movieName, movieYear, "Arya", "Stark", "Maisie");
+		controller.addPublisher(movieName, movieYear, "HBO");
+		controller.addRater(movieName, movieYear, "IMDB", raterRating);
 	}
 }//end class
