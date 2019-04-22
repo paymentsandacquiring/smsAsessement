f4b21697b7cb817657ec96f83a2054f90643ec0f
lanzo King <lanzoking@gmail.com>
2019-04-21 21:13:07 -0500

    Not sure what was the last change here

--- Sms-Movie-Lover/src/main/java/com/movie/repository/MovieResults.java
+++ Sms-Movie-Lover/src/main/java/com/movie/repository/MovieResults.java
@@ -39,6 +39,7 @@ public class MovieResults {
 	
 	public void setMovieGenre(String movieGenre) {
 		this.movieGenres.put(movieGenre, movieGenre);
+		this.formattedGenre = "";
 	}
 	
 	public HashMap<String, String> getMovieGenre() {
@@ -47,6 +48,7 @@ public class MovieResults {
 	
 	public void setMoviePublisher(String moviePublisher) {
 		this.moviePublishers.put(moviePublisher, moviePublisher);
+		this.formattedPublisher = "";
 	}
 	
 	public HashMap<String, String> getMoviePublisher() {
@@ -56,15 +58,15 @@ public class MovieResults {
 	public String formatGenre() {
 		
 		this.movieGenres.forEach((k, v)->{
-			formattedGenre += this.movieGenres.get(k)+", ";
+			formattedGenre += this.movieGenres.get(v)+", ";
 		});
 		formattedGenre = formattedGenre.substring(0, formattedGenre.length() - 2);
 		return formattedGenre;
 	}
 	public String formatPublisher() {
-		
+		System.out.println(moviePublishers);
 		this.moviePublishers.forEach((k, v)->{
-			formattedPublisher += this.moviePublishers.get(k)+", ";
+			formattedPublisher += this.moviePublishers.get(v)+", ";
 		});
 		formattedPublisher = formattedPublisher.substring(0, formattedPublisher.length() - 2);
 		return formattedPublisher;
